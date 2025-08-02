132fcc68ac24a0aacaa182a6999b9a48c0e9be3a
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2025-08-02 18:01:29 +0100

    refactor: organize project structure and clean up

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,79 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const COMPILER_SETTINGS = {
+  optimizer: {
+    enabled: true,
+    runs: 1000000,
+  },
+  metadata: {
+    bytecodeHash: "none",
+  },
+};
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: COMPILER_SETTINGS,
+      },
+      {
+        version: "0.8.20",
+        settings: COMPILER_SETTINGS,
+      },
+      {
+        version: "0.8.7",
+        settings: COMPILER_SETTINGS,
+      },
+      {
+        version: "0.8.6",
+        settings: COMPILER_SETTINGS,
+      },
+      {
+        version: "0.8.0",
+        settings: COMPILER_SETTINGS,
+      },
+    ],
+  },
+  networks: {
+    holesky: {
+      url: process.env.HOLESKY_RPC_URL || "",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [""],
+    },
+    coretestnet: {
+      url: process.env.CORE_TESTNET_RPC_URL || "",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [""],
+      chainId: 1114,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      holesky: process.env.HOLESKY_ETHERSCAN_KEY,
+      coretestnet: process.env.CORE_SCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "holesky",
+        chainId: 17000,
+        urls: {
+          apiURL: "https://holesky.etherscan.io/api",
+          browserURL: "https://holesky.etherscan.io",
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 20000,
+  },
+};
